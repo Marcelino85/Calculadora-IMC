3551a95eb45072de5ce8034e6066cf6a80091837
Marcelino Albuquerque <marcelino85albuquerque@gmail.com>
2023-03-06 12:57:42 -0300

    Atualização do JS e inclusão do CSS

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
-const button = document.getElementById('button');
-const nome = document.getElementById('nome').value;
-const peso = document.getElementById('peso').value;
-const altura = +document.getElementById('altura').value;
-const resultado = document.getElementById('resultado');
+
+const button = document.getElementById('button')
+const nome = document.getElementById('nome')
+const peso = document.getElementById('peso')
+const altura = document.getElementById('altura')
+const resultado = document.getElementById('resultado')
 
 const getText = (imc) => {
-    if(imc > 40) return 'Obesidade grau III'
-    if(imc > 35) return 'Obesidade grau II'
-    if(imc > 30) return 'Obesidade grau I'
-    if(imc > 25) return 'Levemente acima do Peso'
-    if(imc > 18.5) return 'Peso Ideal'
-    return 'Abaixo do Peso'
+    if(imc>40) return 'Obesidade grau III'
+    if(imc>35) return 'Obesidade grau II'
+    if(imc>30) return 'Obesidade grau I'
+    if(imc>25) return 'Levemente acima do peso'
+    if(imc>18.5) return 'peso ideal!'
+    return 'Abaixo do peso'
 }
 
+
 const imcCalc = () => {
-    const valorImc = (peso / (altura * altura)).toFixed(1);
-    resultado.textContent = getText(valorImc);
+    const valorImc = (+peso.value / (+altura.value * +altura.value)).toFixed(1)
+    resultado.textContent = `${nome.value} - ${getText(valorImc)}`
 }
 
 button.addEventListener('click', imcCalc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const button = document.getElementById('button');
+// const nome = document.getElementById('nome').value;
+// const peso = document.getElementById('peso').value;
+// const altura = +document.getElementById('altura').value;
+// const resultado = document.getElementById('resultado');
+
+// const getText = (imc) => {
+//     if(imc > 40) return 'Obesidade grau III'
+//     if(imc > 35) return 'Obesidade grau II'
+//     if(imc > 30) return 'Obesidade grau I'
+//     if(imc > 25) return 'Levemente acima do Peso'
+//     if(imc > 18.5) return 'Peso Ideal'
+//     return 'Abaixo do Peso'
+// }
+
+// const imcCalc = () => {
+//     const valorImc = (peso / (altura * altura)).toFixed(1);
+//     resultado.textContent = getText(valorImc);
+// }
+
+// button.addEventListener('click', imcCalc)
